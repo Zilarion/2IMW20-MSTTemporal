0d425a2d3f6d27ab0485c148aeba58569981e733
Ruud Andriessen <r.andriessen@ilysian.com>
2016-05-28 17:27:34 +0200

    made MSTwNew compile for now, updated it to use explicit vertices/edges aswell as updating it to use DSTs

--- src/computation/MSTwNew.java
+++ src/computation/MSTwNew.java
@@ -1,8 +1,7 @@
 package computation;
 
 import model.*;
-import treemodel.Node;
-import treemodel.Tree;
+import dst.DST;
 
 import java.util.List;
 
@@ -13,7 +12,9 @@ public class MSTwNew extends Algorithm {
     @Override
     public void run(AbstractGraph graph) {
         if (graph instanceof TemporalGraph) {
-            // ok
+            TemporalGraph g = (TemporalGraph) graph;
+//            dst = transform(g)
+//            huang(dst)
         } else {
             throw new IllegalArgumentException("Cannot use MSTw without temporal graph");
         }
@@ -23,57 +24,57 @@ public class MSTwNew extends Algorithm {
      * Calculates MSTw according to the improved algorithm proposed by Huang
      * @param k The number of covered terminals
      * @param i The height of the tree
-     * @param root The root of the tree
+     * @param r The root of the tree
      * @param X Terminal set
      */
-    private void huang(int k, int i, Vertex r, List<AbstractVertex> X, Graph g) {
-        Tree T = new Tree();
-        if (i == 1) {
-            while (k > 0) {
-                // (r,v) <- arg_(r,v) min cost(r, v) FORALL v in X
-                float minCost = Float.MAX_VALUE;
-                AbstractEdge minEdge = null; // (r,v)
-                for (AbstractVertex v : X) { // For all v : X get v with min cost
-                    AbstractEdge e = r.getOutEdge(v);
-                    if (e.weight() < minCost) {
-                        minEdge = e;
-                        minCost = e.weight();
-                    }
-                }
-
-                // add (r,v) to T
-                T.add(minEdge);
-
-                // k <- k - 1
-                k -= 1;
-
-                // remove v from x
-                X.remove(minEdge.to());
-            }
-        } else {
-            while (k > 0) {
-                // TBest <- empty density(TBest) = infinity
-                Tree TBest = null;
-                float bestDensity = Float.MAX_VALUE;
-
-                // foreach vertex v in V do
-                for (AbstractVertex v : g.getVertices().values()) {
-                    // Get (r,v)
-                    AbstractEdge e = r.getOutEdge(v);
-                    // Call other algorithm
-                    Tree TPrime = huangB(i-1, k, v, X, e);
-
-                    float TPrimeDensity = TPrime.density();
-                    if (bestDensity > TPrimeDensity) {
-                        bestDensity = TPrimeDensity;
-                    }
-
-                    T.merge(TBest);
-                    k = k - X.size() INTERSECT V(TBest);
-                    X.removeAll(TBest.vertices());
-                }
-            }
-        }
+    private DST huang(int k, int i, TemporalVertex r, List<TemporalVertex> X, TemporalGraph g) {
+        DST T = new DST();
+//        if (i == 1) {
+//            while (k > 0) {
+//                // (r,v) <- arg_(r,v) min cost(r, v) FORALL v in X
+//                float minCost = Float.MAX_VALUE;
+//                TemporalEdge minEdge = null; // (r,v)
+//                for (TemporalVertex v : X) { // For all v : X get v with min cost
+//                    TemporalEdge e = r.getOutEdge(v);
+//                    if (e.weight() < minCost) {
+//                        minEdge = e;
+//                        minCost = e.weight();
+//                    }
+//                }
+//
+//                // add (r,v) to T
+//                T.add(minEdge);
+//
+//                // k <- k - 1
+//                k -= 1;
+//
+//                // remove v from x
+//                X.remove(minEdge.to());
+//            }
+//        } else {
+//            while (k > 0) {
+//                // TBest <- empty density(TBest) = infinity
+//                DST TBest = null;
+//                float bestDensity = Float.MAX_VALUE;
+//
+//                // foreach vertex v in V do
+//                for (TemporalVertex v : g.getVertices().values()) {
+//                    // Get (r,v)
+//                    TemporalEdge e = r.getOutEdge(v);
+//                    // Call other algorithm
+//                    DST TPrime = huangB(i-1, k, v, X, e);
+//
+//                    float TPrimeDensity = TPrime.density();
+//                    if (bestDensity > TPrimeDensity) {
+//                        bestDensity = TPrimeDensity;
+//                    }
+//
+//                    T.merge(TBest);
+//                    k = k - X.size() INTERSECT V(TBest);
+//                    X.removeAll(TBest.vertices());
+//                }
+//            }
+//        }
         return T;
     }
 
@@ -81,12 +82,13 @@ public class MSTwNew extends Algorithm {
      *
      * @param i Level number
      * @param k Maximum number of available terminals
-     * @param r Tree root
+     * @param r DST root
      * @param X Terminal set
      * @param e The incoming edge of r
      * @return : A tree T with height i rooted at r covering at most k terminals in X so that the density of T U e is the smallest
      */
-    private void huangB(int i, int k, Vertex r, List<AbstractVertex> X, AbstractEdge e) {
+    private DST huangB(int i, int k, TemporalVertex r, List<TemporalVertex> X, TemporalEdge e) {
+        DST dst = new DST();
         if (i == 1) {
             while (k > 0) {
 
@@ -94,5 +96,6 @@ public class MSTwNew extends Algorithm {
         } else {
 
         }
+        return dst;
     }
 }
