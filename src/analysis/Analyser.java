9040f33ffd4c1a757d6cf4ccefaf9602220b23d1
MinskNasa <hu0010@hotmail.com>
2016-06-02 14:51:27 +0200

    Add files via upload

--- src/analysis/Analyser.java
+++ src/analysis/Analyser.java
@@ -1,17 +1,33 @@
 package analysis;
 
 import computation.Algorithm;
+import java.time.Clock;
+import java.util.ArrayList;
 import model.AbstractGraph;
 
 /**
  * Created by ruudandriessen on 11/05/16.
  */
 public class Analyser {
+
     /**
      * Analyses the results of a given algorithm using a certain graph
+     *
      * @param alg The algorithm to use
      * @param graph The graph to analyse
      */
-    public void analyse(Algorithm alg, AbstractGraph graph){
+    long pretime, runtime;
+    private Clock clock;
+
+    public void analyse(ArrayList<Algorithm> alg, ArrayList<AbstractGraph> graph) {
+
+        for (Algorithm instalo : alg) {
+            for (AbstractGraph instgraph : graph) {
+                pretime = clock.millis();
+                instalo.run(instgraph);
+                runtime = clock.millis() - pretime;
+                System.out.println("Runtime of Algorithm" + instalo + "on the graph" + instgraph + "is" + runtime + "miliseconds");
+            }
+        }
     }
 }
